a3df6b485bd1c1b1b1bf6d460fc02cdff37ec60f
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-02-19 16:31:58 -0500

    Generics

---
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,35 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> items = new ArrayList<>();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+      //  items.add("Apple");
+
+
+        printDoubled(items);
+
+
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (Integer each : items) {
+            System.out.println(each*2);
+        }
+
+
+
+
+    }
+
+
+}
