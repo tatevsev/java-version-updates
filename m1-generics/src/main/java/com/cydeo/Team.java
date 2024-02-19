d4ff2b04fe33ef6630078be0c65953541a447f67
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-02-19 17:02:04 -0500

    Generics

---
+++ m1-generics/src/main/java/com/cydeo/Team.java
@@ -0,0 +1,34 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Team {
+
+    private String name;
+
+    private List<Player> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(Player player){
+        if(members.contains(player)){
+            System.out.println(player.getName() + " is already on the team");
+            return false;
+        } else{
+            members.add(player);
+            System.out.println(player.getName() + " picked for a team " + this.name);
+            return true;
+        }
+
+    }
+
+
+}
