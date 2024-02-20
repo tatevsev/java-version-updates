0e4a878ead13389a16b5c37e5b1a1235f7c2dee1
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-02-19 20:32:27 -0500

    Generics

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -1,9 +1,18 @@
 package com.cydeo;
 
 public enum Currency {
-    PENNY,DIME,QUARTER,NICKLE
+    PENNY(1),DIME(10),QUARTER(25),NICKLE(5);
 
+   private int value;
+
+    Currency(int value) {
+        this.value = value;
+    }
 
     //PENNY,DIME,QUARTER,NICKLE are constant objects of Currency class
 
+
+    public int getValue() {
+        return value;
+    }
 }
--- m2-enum/src/main/java/com/cydeo/Operation.java
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -1,6 +1,9 @@
 package com.cydeo;
 
 public enum Operation {
-    PLUS,MINUS,MULTIPLY,DIVIDE
+    PLUS,MINUS,MULTIPLY,DIVIDE; //each constant is an object of Operation
 
+    Operation() { //constructor cannot be public
+        System.out.println("Constructor");
+    }
 }
