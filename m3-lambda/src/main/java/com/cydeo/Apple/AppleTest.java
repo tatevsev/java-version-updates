8f22e8f38b73ec1a15e8d4f842630303acd52887
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-02-23 02:22:24 -0500

    Lambda

--- m3-lambda/src/main/java/com/cydeo/Apple/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/Apple/AppleTest.java
@@ -13,6 +13,10 @@ public class AppleTest {
         inventory.add(new Apple(200,Color.GREEN));
         inventory.add(new Apple(50,Color.RED));
 
+        //FOR EACH METHOD to print elements of Collection using LAMBDA
+        inventory.forEach(apple -> System.out.println(apple));
+
+
         List<Apple> heavyApple  = filterApples(inventory,new AppleHeavyPredicate());
         System.out.println(heavyApple);
         List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
