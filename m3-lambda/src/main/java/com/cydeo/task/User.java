a9706f1a557817d29b6e4dd3eeb01a93a13f9644
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-02-25 10:14:30 -0500

    Lambda

---
+++ m3-lambda/src/main/java/com/cydeo/task/User.java
@@ -0,0 +1,13 @@
+package com.cydeo.task;
+
+import lombok.*;
+
+
+@AllArgsConstructor
+@Data
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+}
