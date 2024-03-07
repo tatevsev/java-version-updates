1fdd6f811dd4d48ab56b781ebd0b71a9aa4ea22f
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-03-06 20:17:58 -0500

    Switch case updates, JAVA 14

--- m8-new-version-updates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
@@ -37,26 +37,27 @@ public class SwitchCaseDemo {
                 System.out.println("***************************");
 
                 int day = 0;
-                switch (month){
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                day = 31;
-                break;
-            case 4:
-            case 6:
-            case 9:
-                day = 30;
-                break;
-            case 2:
-                day = 28;
-                break;
-            default:
-               throw new IllegalArgumentException("invalid month");
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        day = 31;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                        day = 30;
+                        break;
+                    case 2:
+                        day = 28;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("invalid month");
+                }
 
                 System.out.println("***************************");
 
