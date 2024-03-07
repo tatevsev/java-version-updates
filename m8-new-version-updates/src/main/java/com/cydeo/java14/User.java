fd58b6194de0d84e8667162d644551e5018c211f
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-03-06 23:17:01 -0500

    Record class, JAVA 14

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,12 @@
+package com.cydeo.java14;
+
+public record User(String name,String address) {
+
+    //ALL ARGS CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+    //each field is priavte final therefore there is no setter
+}
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,11 @@
+package com.cydeo.java14;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+
+        User user1 = new User("Mike","VA");
+        System.out.println(user1.name()); //while creating object from Record class to call states directly can use name() instead of getters
+    }
+}
